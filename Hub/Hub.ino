c8b6497a455ed638cc9532da275c74cb49be50dd
phsete <philteb@gmx.de>
2023-01-14 23:33:55 +0100

    first version of ESP-NOW / HomeSpan based firmware

---
+++ Hub/Hub.ino
@@ -0,0 +1,139 @@
+
+/*********************************************************************************
+ *  MIT License
+ *  
+ *  Copyright (c) 2022 Philipp Tebbe
+ *  
+ *  https://github.com/phsete/planty-firmware
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ *  
+ ********************************************************************************
+ *
+ *  This script uses the HomeSpan Library by Gregg E. Berman and is based on the 
+ *  RemoteSensor example.
+ *  Check it out at https://github.com/HomeSpan/HomeSpan !
+ *  For this script to work you have to install the library first. For instructions
+ *  please have alook at the stated github link.
+ *
+ ********************************************************************************/
+
+#include "HomeSpan.h"         // include the HomeSpan library
+
+struct BatteryMeasurement : Service::BatteryService {
+  SpanPoint *remoteSensor;
+  SpanCharacteristic *batteryLevel;
+  SpanCharacteristic *chargingState;
+  SpanCharacteristic *statusLowBattery;
+  const char *name;
+  String receivedData;
+  float *batteryReceived;
+  bool *newBatteryAvailable;
+
+  BatteryMeasurement(const char *name, SpanPoint *remoteSensor, bool *newBatteryAvailable, float *batteryReceived) : Service::BatteryService() {
+    batteryLevel = new Characteristic::BatteryLevel();
+    chargingState = new Characteristic::ChargingState();
+    statusLowBattery = new Characteristic::StatusLowBattery();
+
+    this->name=name;
+    this->remoteSensor=remoteSensor;
+    this->newBatteryAvailable=newBatteryAvailable;
+    this->batteryReceived=batteryReceived;
+  }
+
+  void loop() {
+    if(*newBatteryAvailable){      // if there is data from the remote sensor
+      LOG1("BATT: %d\n", *batteryReceived);
+      batteryLevel->setVal(*batteryReceived/4095*100);            // update temperature
+       
+      LOG1("Sensor %s update: Battery=%0.2f\n",name,*batteryReceived/4095*100);
+      *newBatteryAvailable = false;
+    }
+  }
+};
+
+struct SoilMoisture : Service::HumiditySensor {
+  SpanPoint *remoteSensor;
+  SpanCharacteristic *soilMoistureLevel;
+  const char *name;
+  float soilMoistureValue;
+  String receivedData;
+  float *batteryReceived;
+  bool *newBatteryAvailable;
+ 
+  SoilMoisture(const char *name, SpanPoint *remoteSensor, bool *newBatteryAvailable, float *batteryReceived) : Service::HumiditySensor() {
+    soilMoistureLevel = new Characteristic::CurrentRelativeHumidity();
+
+    this->name=name;
+    this->remoteSensor=remoteSensor;
+    this->newBatteryAvailable=newBatteryAvailable;
+    this->batteryReceived=batteryReceived;
+  }
+
+  void loop() {
+    if(remoteSensor->get(&receivedData)){      // if there is data from the remote sensor
+      LOG1("received: %s\n", receivedData.c_str());
+      int index = receivedData.indexOf('|');
+      soilMoistureValue = receivedData.substring(0, index).toInt();
+      *batteryReceived = receivedData.substring(index+1, receivedData.length()).toInt();
+      *newBatteryAvailable = true;
+      LOG1("SOIL: %d\n", soilMoistureValue);
+      soilMoistureLevel->setVal(soilMoistureValue/4095*100);            // update temperature
+        
+      LOG1("Sensor %s update: Soil-Moisture=%0.2f\n",name,soilMoistureValue/4095*100);
+    }
+  }
+};
+
+struct RemoteSensor {
+  SpanPoint *remoteSensor;
+  float batteryReceived;
+  bool newBatteryAvailable = false;
+  RemoteSensor(const char *name, const char *macAddress) {
+    new SpanAccessory();
+    remoteSensor=new SpanPoint(macAddress,0,sizeof(String));
+    new Service::AccessoryInformation();
+      new Characteristic::Identify();
+      new Characteristic::Name(name);
+    new SoilMoisture(name, remoteSensor, &newBatteryAvailable, &batteryReceived);
+    new BatteryMeasurement(name, remoteSensor, &newBatteryAvailable, &batteryReceived);
+  }
+};
+
+void setup() {     
+ 
+  Serial.begin(115200);       // start the Serial interface
+
+  homeSpan.setLogLevel(1);
+
+  homeSpan.begin(Category::Bridges,"Sensor Hub");
+
+  new SpanAccessory();  
+    new Service::AccessoryInformation();
+      new Characteristic::Identify(); 
+
+  new RemoteSensor("Planty 1","84:0d:8e:07:e3:4c");
+  
+} // end of setup()
+
+void loop(){
+
+  homeSpan.poll();
+
+} // end of loop()
---
+++ Remote/Remote.ino
@@ -0,0 +1,71 @@
+
+/*********************************************************************************
+ *  MIT License
+ *  
+ *  Copyright (c) 2022 Philipp Tebbe
+ *  
+ *  https://github.com/phsete/planty-firmware
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ *  
+ ********************************************************************************
+ *
+ *  This script uses the HomeSpan Library by Gregg E. Berman and is based on the 
+ *  RemoteSensor example.
+ *  Check it out at https://github.com/HomeSpan/HomeSpan !
+ *  For this script to work you have to install the library first. For instructions
+ *  please have alook at the stated github link.
+ *
+ ********************************************************************************/
+
+#include "HomeSpan.h"
+
+#define SAMPLE_TIME   30000     // Time between samples (in milliseconds)
+
+SpanPoint *mainDevice;
+
+void setup() {
+
+  setCpuFrequencyMhz(80);       // reduce CPU frequency to save battery power  
+  
+#if defined(DIAGNOSTIC_MODE)  
+  homeSpan.setLogLevel(1);
+  Serial.begin(115200);
+  delay(1000);
+  Serial.printf("Starting Remote Sensor.  MAC Address of this device = %s\n",WiFi.macAddress().c_str());
+#endif
+
+  // In the line below, replace the MAC Address with that of your MAIN HOMESPAN DEVICE
+
+  mainDevice=new SpanPoint("24:6F:28:22:A3:1C",sizeof(String),0);    // create a SpanPoint with send size=sizeof(float) and receive size=0     
+}
+
+void loop() {
+  int soilMoistureValue = analogRead(33);
+  int batteryValue = analogRead(32);
+  String dataToSend = "|"; // SOILMOISTURE|BATTERY - Werte zwischen 0 und 4095
+  dataToSend = soilMoistureValue + dataToSend;
+  dataToSend = dataToSend + batteryValue;
+
+  boolean success = mainDevice->send(&dataToSend);                 // this will show as success as long as the MAIN DEVICE is running
+#if defined(DIAGNOSTIC_MODE)  
+  Serial.printf("Send %s\n",success?"Succeded":"Failed");
+#endif
+  esp_deep_sleep(SAMPLE_TIME*1000);     // enter deep sleep mode -- reboot after resuming
+}
